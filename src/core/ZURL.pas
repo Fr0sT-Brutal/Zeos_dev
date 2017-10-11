Fr0sT-Brutal/Zeos_dev

commit 4262b3c9891e75a3339f923cf9e5118f38d59674

* Refactoring of TZURL.SetURL, code shortened and simplified + make possible specifying full paths with ":" in parameters (SF #158). Some tests added
git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.2@4028 d681b350-854c-439c-a81d-c0ce20710203

src/core/ZURL.pas
@@ -256,12 +256,10 @@ function TZURL.GetURL: string;
 var
   hasParamPart : boolean;
   procedure AddParamPart(const ParamPart: String);
+  const
+    ParamSep: array[Boolean] of Char = (';', '?');
   begin
-    if hasParamPart then
-      Result := Result + ';'
-    else
-      Result := Result + '?';
-    Result := Result + ParamPart;
+    Result := Result + ParamSep[hasParamPart] + ParamPart;
     hasParamPart := True;
   end;
 
@@ -302,8 +300,8 @@ begin
     AddParamPart(Properties.GetURLText); //Adds the escaped string
 
   // LibLocation
-    if FLibLocation <> '' then
-      AddParamPart('LibLocation='+ FLibLocation);
+  if FLibLocation <> '' then
+    AddParamPart('LibLocation='+ FLibLocation);
 end;
 
 procedure TZURL.SetURL(const Value: string);
@@ -331,6 +329,15 @@ begin
   try
     AValue := Value;
 
+    // Strip out the parameters
+    I := ZFastCode.Pos('?', AValue);
+    if I > 0 then
+    begin
+      AValue := Copy(AValue, I + 1, MaxInt);
+      AProperties.Text := StringReplace(AValue, ';', LineEnding, [rfReplaceAll]);
+      AValue := Copy(Value, 1, I - 1);
+    end;
+
     // APrefix
     I := ZFastCode.Pos(':', AValue);
     if I = 0 then
@@ -349,47 +356,35 @@ begin
     if ZFastCode.Pos('//', AValue) = 1 then
     begin
       Delete(AValue, 1, 2);
-      if (ZFastCode.Pos(':', AValue) > 0) and
-         ((ZFastCode.Pos(':', AValue) < ZFastCode.Pos('/', AValue)) or
-         (ZFastCode.Pos('/', AValue)=0)) then
-        AHostName := Copy(AValue, 1, ZFastCode.Pos(':', AValue) - 1)
-      else if ZFastCode.Pos('/', AValue) > 0 then
-        AHostName := Copy(AValue, 1, ZFastCode.Pos('/', AValue) - 1)
-      else if ZFastCode.Pos('?', AValue) > 0 then
-        AHostName := Copy(AValue, 1, ZFastCode.Pos('?', AValue) - 1)
-      else
-        AHostName := AValue;
 
-      Delete(AValue, 1, Length(AHostName));
+      // Strip "hostname[:port]" out of "/database"
+      I := ZFastCode.Pos('/', AValue);
+      if I > 0 then
+      begin
+        AHostName := Copy(AValue, 1, I - 1);
+        Delete(AValue, 1, I);    
+      end
+      else
+      begin
+        AHostName := AValue;
+        AValue := '';
+      end;
 
       // APort
-      if ZFastCode.Pos(':', AValue) = 1 then
+      I := ZFastCode.Pos(':', AHostName);
+      if I > 0 then
       begin
-        Delete(AValue, 1, 1);
-        if ZFastCode.Pos('/', AValue) > 0 then
-          APort := Copy(AValue, 1, ZFastCode.Pos('/', AValue) - 1)
-        else if ZFastCode.Pos('?', AValue) > 0 then
-          APort := Copy(AValue, 1, ZFastCode.Pos('?', AValue) - 1)
-        else
-          APort := AValue;
-
-        Delete(AValue, 1, Length(APort));
+        APort := Copy(AHostName, I + 1, MaxInt);
+        Delete(AHostName, I, MaxInt);  
       end;
-    end;
-
+    end
+    else
+    // Likely a database delimited by / so remove the /
     if ZFastCode.Pos('/', AValue) = 1 then
       Delete(AValue, 1, 1);
 
     // ADatabase
-    I := ZFastCode.Pos('?', AValue);
-    if I > 0 then
-    begin
-      ADatabase := Copy(AValue, 1, I - 1);
-      Delete(AValue, 1, I);
-      AProperties.Text := StringReplace(AValue, ';', LineEnding, [rfReplaceAll]);
-    end
-    else
-      ADatabase := AValue;
+    ADatabase := AValue;
 
     FPrefix := APrefix;
     FProtocol := AProtocol;

test/core/ZTestURL.pas
@@ -69,7 +69,7 @@ type
   published
     procedure TestAssignToUrl;
     procedure TestAssignToProperties;
-    procedure TestAssignToProperties_DatabaseIsFile;
+    procedure TestAssignToUrl_DatabaseIsFile;
     procedure TestAssignToProperties_NoHostUserPwd;
     procedure TestAssignToProperties_NoUser;
     procedure TestAssignToProperties_Properties;
@@ -83,7 +83,10 @@ type
     procedure TestAssignToProperties_Properties_NoHost;
     procedure TestSemicolons;
     procedure TestUnixPathDelimiter;
+    procedure TestAssignToUrl_NoParams;
+    procedure TestAssignToUrl_NoDatabase;
     procedure TestSFTicket8_HostPort_NoDB_Properties;
+    procedure TestSFTicket158_LibLocWithSemicolon;
   end;
 
 implementation
@@ -308,7 +311,7 @@ begin
   end;
 end;
 
-procedure TZURLTest.TestAssignToProperties_DatabaseIsFile;
+procedure TZURLTest.TestAssignToUrl_DatabaseIsFile;
 var
   ZURL: TZURL;
 begin
@@ -444,6 +447,41 @@ begin
   end;
 end;
 
+procedure TZURLTest.TestAssignToUrl_NoParams;
+var
+  ZURL: TZURL;
+begin
+  // Test assignment to URL without params
+  ZURL := nil;
+  try
+    ZURL := TZURL.Create;
+    ZURL.URL := 'zdbc:firebird-2.0:/database.fdb';
+    CheckEquals('zdbc', ZURL.Prefix);
+    CheckEquals('firebird-2.0', ZURL.Protocol);
+    CheckEquals('', ZURL.HostName);
+    CheckEquals(0, ZURL.Port);
+    CheckEquals('database.fdb', ZURL.Database);
+    CheckEquals('', ZURL.Properties.Text);
+  finally
+    ZURL.Free;
+  end;
+  // Test assignment to URL without params and ? sign
+  ZURL := nil;
+  try
+    ZURL := TZURL.Create;
+    ZURL.URL := 'zdbc:firebird-2.0:/database.fdb?';
+    CheckEquals('zdbc', ZURL.Prefix);
+    CheckEquals('firebird-2.0', ZURL.Protocol);
+    CheckEquals('', ZURL.HostName);
+    CheckEquals(0, ZURL.Port);
+    CheckEquals('database.fdb', ZURL.Database);
+    CheckEquals('', ZURL.Properties.Text);
+  finally
+    ZURL.Free;
+  end;
+end;
+
+
 { SourceForge.net Ticket #8:
 TZURL.SetURL(const Value: string); maybe parse error.
 
@@ -468,6 +506,74 @@ begin
     CheckEquals('root', ZURL.UserName);
     CheckEquals('test', ZURL.Password);
     CheckEquals('zdbc:firebird-2.0://localhost:3306?username=root;password=test', ZURL.URL);
+  finally
+    ZURL.Free;
+  end;
+end;
+
+procedure TZURLTest.TestAssignToUrl_NoDatabase;
+var
+  ZURL: TZURL;
+begin
+  // Test assignment to URL without params
+  ZURL := nil;
+  try
+    ZURL := TZURL.Create;
+    ZURL.URL := 'zdbc:firebird-2.0:';
+    CheckEquals('zdbc', ZURL.Prefix);
+    CheckEquals('firebird-2.0', ZURL.Protocol);
+    CheckEquals('', ZURL.HostName);
+    CheckEquals(0, ZURL.Port);
+    CheckEquals('', ZURL.Database);
+    CheckEquals('', ZURL.Properties.Text);
+  finally
+    ZURL.Free;
+  end;
+end;
+
+{ SourceForge.net Ticket #158:
+  Liblocation with ":" breaks parsing
+}
+procedure TZURLTest.TestSFTicket158_LibLocWithSemicolon;
+var
+  ZURL: TZURL;
+begin
+  ZURL := nil;
+  try
+    ZURL := TZURL.Create;
+    ZURL.URL := 'zdbc:firebird-2.0://localhost:3306/database?LibLocation=c:\fbclient.dll';
+    CheckEquals('zdbc', ZURL.Prefix);
+    CheckEquals('firebird-2.0', ZURL.Protocol);
+    CheckEquals('localhost', ZURL.HostName);
+    CheckEquals(3306, ZURL.Port);
+    CheckEquals('database', ZURL.Database);
+    CheckEquals('c:\fbclient.dll', ZURL.LibLocation);
+  finally
+    ZURL.Free;
+  end;
+  ZURL := nil;
+  try
+    ZURL := TZURL.Create;
+    ZURL.URL := 'zdbc:firebird-2.0:///database?LibLocation=c:\fbclient.dll';
+    CheckEquals('zdbc', ZURL.Prefix);
+    CheckEquals('firebird-2.0', ZURL.Protocol);
+    CheckEquals('', ZURL.HostName);
+    CheckEquals(0, ZURL.Port);
+    CheckEquals('database', ZURL.Database);
+    CheckEquals('c:\fbclient.dll', ZURL.LibLocation);
+  finally
+    ZURL.Free;
+  end;
+  ZURL := nil;
+  try
+    ZURL := TZURL.Create;
+    ZURL.URL := 'zdbc:firebird-2.0:///c:\test\database.fdb?LibLocation=c:\fbclient.dll';
+    CheckEquals('zdbc', ZURL.Prefix);
+    CheckEquals('firebird-2.0', ZURL.Protocol);
+    CheckEquals('', ZURL.HostName);
+    CheckEquals(0, ZURL.Port);
+    CheckEquals('c:\test\database.fdb', ZURL.Database);
+    CheckEquals('c:\fbclient.dll', ZURL.LibLocation);
   finally
     ZURL.Free;
   end;

commit 2bcbf7d0ef49cd0063419cdbe96710b025900ea1

Fix bug introduced in r4028 (wrong URL construction)
git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.2@4030 d681b350-854c-439c-a81d-c0ce20710203

src/core/ZURL.pas
@@ -257,7 +257,7 @@ var
   hasParamPart : boolean;
   procedure AddParamPart(const ParamPart: String);
   const
-    ParamSep: array[Boolean] of Char = (';', '?');
+    ParamSep: array[Boolean] of Char = ('?', ';');
   begin
     Result := Result + ParamSep[hasParamPart] + ParamPart;
     hasParamPart := True;